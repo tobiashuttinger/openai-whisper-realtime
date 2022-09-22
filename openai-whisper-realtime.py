95f155bc0abcf15ccb4105adab0623e4657b80a1
Tobias Huttinger <49185519+tobiashuttinger@users.noreply.github.com>
2022-09-22 14:02:00 +0200

    Improved silence detection, added more settings

--- openai-whisper-realtime.py
+++ openai-whisper-realtime.py
@@ -11,8 +11,14 @@ import sys
 # SETTINGS
 MODEL_TYPE="base.en"
 # the model used for transcription. https://github.com/openai/whisper#available-models-and-languages
+LANGUAGE="English"
+# pre-set the language to avoid autodetection
 BLOCKSIZE=24678 
-# this is the fixed chunk size the audio is split into in samples
+# this is the base chunk size the audio is split into in samples. blocksize / 16000 = chunk length in seconds. 
+SILENCE_THRESHOLD=400
+# should be set to the lowest sample amplitude that the speech in the audio material has
+SILENCE_RATIO=100
+# number of samples in one buffer that are allowed to be higher than threshold
 
 
 global_ndarray = None
@@ -36,28 +42,36 @@ async def inputstream_generator():
 async def process_audio_buffer():
 	global global_ndarray
 	async for indata, status in inputstream_generator():
-		#print(abs(indata[-1, -1]))
+		
+		indata_flattened = abs(indata.flatten())
+				
+		# discard buffers that contain mostly silence
+		if(np.asarray(np.where(indata_flattened > SILENCE_THRESHOLD)).size < SILENCE_RATIO):
+			continue
 		
 		if (global_ndarray is not None):
 			global_ndarray = np.concatenate((global_ndarray, indata), dtype='int16')
 		else:
 			global_ndarray = indata
 			
-		if (abs(indata[-1, -1]) > 100):
+		# concatenate buffers if the end of the current buffer is not silent
+		if (np.average((indata_flattened[-100:-1])) > SILENCE_THRESHOLD/15):
 			continue
 		else:
 			local_ndarray = global_ndarray.copy()
 			global_ndarray = None
 			indata_transformed = local_ndarray.flatten().astype(np.float32) / 32768.0
-			result = model.transcribe(indata_transformed, language='English')
+			result = model.transcribe(indata_transformed, language=LANGUAGE)
 			print(result["text"])
-		#if (avgResult = np.average(indata.reshape(-1, n), axis=1))
+			
+		del local_ndarray
+		del indata_flattened
 
 
 async def main():
 	print('\nActivating wire ...\n')
 	audio_task = asyncio.create_task(process_audio_buffer())
-	for i in range(100, 0, -1):
+	while True:
 		await asyncio.sleep(1)
 	audio_task.cancel()
 	try:
